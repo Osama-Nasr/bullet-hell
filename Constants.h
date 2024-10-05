18cc2a9b3bfbf3e523edd77d1cf40b9e0f266c16
Osamah <s188051@student.pg.edu.pl>
2024-10-05 21:52:17 +0200

    first commit

---
+++ Constants.h
@@ -0,0 +1,14 @@
+// Constants.h
+#if !defined(MYLIB_CONSTANTS_H)
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+
+const int numberOfenemies = 7;		// the number of the enemies 
+const int rewardPoints = 5;			// the points taht will be added to the player sscore when it hits somebody
+const int hitDepletedPoints = 2;   //the points that will be taken from the player in case of getting hit
+const int player_speed_init = 5;   //the inial spped of the player that it will start the game with
+const int bullet_speed = 6;			//the spped of the bullets of both enemys and player
+const double timeEveryBonusSEC = 8.0;			//the spped of the bullets of both enemys and player
+const double rehealingAfterAHitSEC = 5.0;			//the spped of the bullets of both enemys and player
+
+#endif
